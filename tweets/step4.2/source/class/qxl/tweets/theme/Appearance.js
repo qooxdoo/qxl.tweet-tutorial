06af03aa2eac90572259df9329f78956def5eacc
Henner Kollmann <henner.kollmann@gmx.de>
2020-05-11 21:34:03 +0200

    all steps are compiling

---
+++ tweets/step4.2/source/class/qxl/tweets/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxl.tweets.theme.Appearance",
+{
+  extend : qx.theme.indigo.Appearance,
+
+  appearances :
+  {
+  }
+});
